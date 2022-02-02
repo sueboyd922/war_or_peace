3a1b8fb558067c9ce3e99d6da0e76a54ec603d70
Susan Boyd <sueboyd922@gmail.com>
2022-02-01 22:11:48 -0500

    Add high ranking cards method

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,16 +1,44 @@
 require './card'
 
 class Deck
-  attr_reader :cards
+  attr_reader :cards 
+  #attr_reader :high_cards
 
   def initialize(cards)
     @cards = cards
+    @high_cards = []
   end
 
   def rank_of_card_at(x)
     cards[x].rank
   end
-  
+
+  def high_ranking_cards
+    cards.select{|card| card.rank > 10}
+    # @high_cards = []
+    # cards.select{
+    #   |card| if card.rank > 10
+    #   high_cards << "#{card.value} of #{card.suit}"
+    # end}
+    # @high_cards
+
+    # high_cards = []
+    # cards.each do |card|
+    #   if card.rank > 10
+    #     high_cards << card
+    #   end
+    # end
+    # high_cards
+
+    # cards.map do |card|
+    #   if card.rank > 10
+    #     card
+    #   end
+    # end
+
+  end
+
+
 require 'pry'; binding.pry
 
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -25,5 +25,17 @@ describe Deck do
     end
   end
 
+  # describe 'high_ranking_cards' do
+  #   it 'tells you the high cards in the deck' do
+  #   card1 = Card.new(:diamond, 'Queen', 12)
+  #   card2 = Card.new(:spade, '3', 3)
+  #   card3 = Card.new(:heart, 'Ace', 14)
+  #   cards = [card1, card2, card3]
+  #   deck = Deck.new(cards)
+  #
+  #   expect(deck.high_ranking_cards).to eq()
+  #   end
+  # end
+
 
 end
