830465239612b73b0158dac7778450b28aa6ebc0
Susan Boyd <sueboyd922@gmail.com>
2022-02-01 21:43:04 -0500

    Add rank of card method

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,11 +1,16 @@
 require './card'
 
 class Deck
+  attr_reader :cards
 
   def initialize(cards)
     @cards = cards
   end
 
-
+  def rank_of_card_at(x)
+    cards[x].rank
+  end
+  
+require 'pry'; binding.pry
 
 end
