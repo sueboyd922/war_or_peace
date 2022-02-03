dab3f7a0a8813ff41f49f8aaf4838f4012130da4
Susan Boyd <sueboyd922@gmail.com>
2022-02-02 21:53:57 -0500

    Fix test for award spoils

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -143,13 +143,44 @@ describe Turn do
 
       turn = Turn.new(player1, player2)
       winner = turn.winner
-      
+
       turn.pile_cards
       turn.award_spoils(winner)
       expect(player1.deck.cards.count).to eq(5)
       expect(player2.deck.cards.count).to eq(3)
       expect(turn.spoils_of_war.count).to eq(0)
+      require 'pry'; binding.pry
+    end
 
+    it 'can play a second turn' do
+      card1 = Card.new(:heart, 'Jack', 11)
+      card2 = Card.new(:heart, '10', 10)
+      card3 = Card.new(:heart, '9', 9)
+      card4 = Card.new(:diamond, 'Jack', 11)
+      card5 = Card.new(:heart, '8', 8)
+      card6 = Card.new(:diamond, 'Queen', 12)
+      card7 = Card.new(:heart, '3', 3)
+      card8 = Card.new(:diamond, '2', 2)
+
+      deck1 = Deck.new([card1, card4, card5, card8])
+      deck2 = Deck.new([card3, card2, card6, card7])
+
+      player1 = Player.new('Megan', deck1)
+      player2 = Player.new('Aurora', deck2)
+
+      turn = Turn.new(player1, player2)
+      winner = turn.winner
+
+      turn.pile_cards
+      turn.award_spoils(winner)
+
+      turn2 = Turn.new(player1, player2)
+      winner2 = turn2.winner
+      turn2.pile_cards
+      turn2.award_spoils(winner2)
+
+      expect(player1.deck.cards.count).to eq(6)
+      expect(player2.deck.cards.count).to eq(2)
     end
   end
 end
