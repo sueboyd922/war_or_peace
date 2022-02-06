1402023340f349fba8a57270a5f52795a6efd876
Susan Boyd <sueboyd922@gmail.com>
2022-02-06 11:26:56 -0500

    Finalize player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,7 @@
 require './lib/deck'
 
 class Player
+  #initialize an object of Player class with readable attributes of name and deck
   attr_reader :name, :deck
 
   def initialize(name, deck)
@@ -8,6 +9,7 @@ class Player
     @deck = deck
   end
 
+  # method to determine if player has lost the game: if their cards array is empty they have lost
   def has_lost?
     @has_lost = deck.cards.empty?
   end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,41 +1,30 @@
 require 'rspec'
-require './lib/deck'
-require './lib/card'
 require './lib/player'
 
-#3 tests total
+# 4 tests total
+card1 = Card.new(:diamond, 'Queen', 12)
+card2 = Card.new(:spade, '3', 3)
+card3 = Card.new(:heart, 'Ace', 14)
+deck = Deck.new([card1, card2, card3])
+player = Player.new('Clarisa', deck)
+
 describe Player do
   describe '#initialize' do
     it 'it is an instance of Player class' do
-      card1 = Card.new(:diamond, 'Queen', 12)
-      card2 = Card.new(:spade, '3', 3)
-      card3 = Card.new(:heart, 'Ace', 14)
-      deck = Deck.new([card1, card2, card3])
-      player = Player.new('Clarisa', deck)
-
       expect(player).to be_an_instance_of(Player)
     end
 
     it 'has a name' do
-      card1 = Card.new(:diamond, 'Queen', 12)
-      card2 = Card.new(:spade, '3', 3)
-      card3 = Card.new(:heart, 'Ace', 14)
-      deck = Deck.new([card1, card2, card3])
-      player = Player.new('Clarisa', deck)
-
       expect(player.name).to eq('Clarisa')
+    end
 
+    it 'has a deck of cards' do
+      expect(player.deck.cards).to eq([card1, card2, card3])
     end
   end
 
   describe '#has_lost' do
     it 'can tell you if the player has lost the game' do
-      card1 = Card.new(:diamond, 'Queen', 12)
-      card2 = Card.new(:spade, '3', 3)
-      card3 = Card.new(:heart, 'Ace', 14)
-      deck = Deck.new([card1, card2, card3])
-      player = Player.new('Clarisa', deck)
-
       expect(player.has_lost?).to be false
       player.deck.remove_card
       expect(player.has_lost?).to be false
@@ -45,10 +34,4 @@ describe Player do
       expect(player.has_lost?).to be true
     end
   end
-
-
-
-
-
-
 end
