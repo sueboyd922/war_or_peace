4451bb9543dc0958b21fbcebd6396706f63ea7a7
Susan Boyd <sueboyd922@gmail.com>
2022-02-06 11:54:03 -0500

    Finalize turn class

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,8 +1,7 @@
-require './lib/card'
-require './lib/deck'
 require './lib/player'
 
 class Turn
+  # initialize an Turn object with two players and two piles (both start empty)
   attr_reader :player1, :player2, :spoils_of_war, :discard_pile
 
   def initialize(player1, player2)
@@ -12,56 +11,58 @@ class Turn
     @discard_pile = []
   end
 
+  # method to determine the type of turn: basic, war, mad
   def type
-    if @player1.has_lost? || @player2.has_lost?
-      if @player1.has_lost?
-        puts "#{@player2.name} wins!"
-      else
-        puts "#{@player1.name} wins!"
-      end
-    elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
-
-      if @player1.deck.rank_of_card_at(2) != @player2.deck.rank_of_card_at(2)
-        @type = :war
-      else
+    #check if the first two cards are equal first
+    if @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
+      #if they are check if the second two are equal
+      if @player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2)
+        #if they are the type is mad
         @type = :mutually_assured_destruction
+      else
+        #if they aren't the type is war
+        @type = :war
       end
     else
+      #if the first two cards are not equal the type is basic
       @type = :basic
     end
   end
 
+  #method to determine the winner of a turn based on the type of turn
   def winner
-    type
+    type #run type first
     case @type
-    when :basic
+    when :basic #check who has a higher card
       if @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0)
         return @player1
       else
         return @player2
       end
-    when :war
+    when :war #check who has a higher card
       if @player1.deck.rank_of_card_at(2) > @player2.deck.rank_of_card_at(2)
         return @player1
       else
         return @player2
       end
-    when :mutually_assured_destruction
+    when :mutually_assured_destruction # everyone loses
+        "No winner"
     end
   end
 
+  # method to remove cards in play from decks
   def pile_cards
-    type
+    type # runs type first to see how many cards are removed
     case @type
-    when :basic
+    when :basic # 1 card each to the spoils of war
       @spoils_of_war << @player1.deck.remove_card
       @spoils_of_war << @player2.deck.remove_card
-    when :war
+    when :war # 3 cards each to the spoils of war
       3.times do
         @spoils_of_war << @player1.deck.remove_card
         @spoils_of_war << @player2.deck.remove_card
       end
-    when :mutually_assured_destruction
+    when :mutually_assured_destruction # 3 cards each to the discard pile
       3.times do
         @discard_pile << @player1.deck.remove_card
         @discard_pile << @player2.deck.remove_card
@@ -69,6 +70,7 @@ class Turn
     end
   end
 
+  # method to give the spoils to the winner
   def award_spoils(winner)
     until @spoils_of_war.empty? == true
       winner.deck.cards << @spoils_of_war.shift
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,7 +1,7 @@
 require 'rspec'
 require './lib/turn'
 
-#11 tests
+#13 tests
 card1 = Card.new(:heart, 'Jack', 11)
 card2 = Card.new(:heart, '10', 10)
 card3 = Card.new(:heart, '9', 9)
@@ -17,12 +17,29 @@ describe Turn do
     deck2 = Deck.new([card3, card4, card6, card7])
     player1 = Player.new('Megan', deck1)
     player2 = Player.new('Aurora', deck2)
-
     turn = Turn.new(player1, player2)
 
     it 'is an instance of the Turn class' do
       expect(turn).to be_an_instance_of(Turn)
     end
+
+    it 'has players' do
+      expect(turn.player1).to eq(player1)
+      expect(turn.player2).to eq(player2)
+    end
+
+    it 'starts out with two empty piles: discard and spoils of war' do
+      expect(turn.spoils_of_war).to eq([])
+      expect(turn.discard_pile).to eq([])
+    end
+  end
+
+  describe "#type" do
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+    player1 = Player.new('Megan', deck1)
+    player2 = Player.new('Aurora', deck2)
+    turn = Turn.new(player1, player2)
 
     it 'it can tell you which type of turn it will be' do
       expect(turn.type).to eq(:basic)
@@ -87,12 +104,10 @@ describe Turn do
 
     it 'has a winner that wins 6 cards' do
       expect(winner.name).to eq('Aurora')
-
       turn.pile_cards
       expect(turn.spoils_of_war).to eq([card1, card4, card3, card2, card5, card6])
 
       turn.award_spoils(winner)
-
       expect(player2.deck.cards).to eq([card7, card1, card4, card3, card2, card5, card6])
       expect(player1.deck.cards).to eq([card8])
       expect(turn.spoils_of_war).to eq([])
@@ -104,10 +119,8 @@ describe Turn do
     card10 = Card.new(:diamond, 'seven', 7)
     deck1 = Deck.new([card1, card3, card9, card5, card8])
     deck2 = Deck.new([card4, card2, card10, card6, card7])
-
     player1 = Player.new('Megan', deck1)
     player2 = Player.new('Aurora', deck2)
-
     turn = Turn.new(player1, player2)
 
     it 'can have have type mutually_assured_destruction' do
@@ -115,11 +128,12 @@ describe Turn do
     end
 
     it 'has no winner' do
-      expect(turn.winner).to eq(nil)
+      expect(turn.winner).to eq("No winner")
     end
 
-    it 'removes all 6 affected cards from play' do
+    it 'discards all 6 cards' do
       turn.pile_cards
+      expect(turn.discard_pile).to eq([card1, card4, card3, card2, card9, card10])
       expect(turn.spoils_of_war).to eq([])
       expect(player1.deck.cards).to eq([card5, card8])
       expect(player2.deck.cards).to eq([card6, card7])
